5587529644c7201580e866a9bd2cac143d14d10f
Luan Seixas <mari04luan@gmail.com>
2025-08-27 11:57:29 -0300

    front: add resposivity in the event cards and fixing navbar on top

--- frontend/src/components/ui/EventList/EventList.tsx
+++ frontend/src/components/ui/EventList/EventList.tsx
@@ -26,7 +26,7 @@ export const EventList = ({ events }: EventListProps) => {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {events.map((event) => (
         <div
           key={event.id}
